ed967803568a0df16a127cf4228b048551b41db6
Marco Di Mambro <148141603+MarcoDiMambro01@users.noreply.github.com>
2024-03-10 13:32:45 +0100

    n

--- vectorized_rxn_net.py
+++ vectorized_rxn_net.py
@@ -594,7 +594,8 @@ class VectorizedRxnNet:
         """
         # above conversions cancel
         # std_c = Tensor([1e6])  # units umols / L
-
+        print("k_on: ",self.kon)
+        print("rn score: ",self.rxn_score_vec)
         #with torch.no_grad():
         l_kon = torch.log(kon)  # umol-1 s-1
         # l_koff = (dGrxn * scalar_modifier / (self._R * self._T)) + l_kon + torch.log(std_c)       #Units of dG in J/mol
