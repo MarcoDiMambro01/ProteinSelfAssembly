c5f6e652448e23d0b65431037fb0c57ed8e0efb7
Marco Di Mambro <148141603+MarcoDiMambro01@users.noreply.github.com>
2024-03-10 18:25:40 +0100

    n

--- optimizer.py
+++ optimizer.py
@@ -347,7 +347,7 @@ class Optimizer:
 
 
                             curr_lr = self.optimizer.state_dict()['param_groups'][0]['lr']
-                            physics_penalty = torch.sum(F.relu(-1 * (k - curr_lr * 10))).to(self.dev) + torch.sum(F.relu(1 * (k - 10))).to(self.dev) # stops zeroing or negating params
+                            physics_penalty = torch.sum(F.relu(-1 * (k - curr_lr * 10))).to(self.dev) #+ torch.sum(F.relu(1 * (k - 10))).to(self.dev) # stops zeroing or negating params
                             #physics_penalty = nn.ReLU(-1 * (k - curr_lr * 10)).to(self.dev) + nn.ReLU(1 * (k - 10)).to(self.dev) # stops zeroing or negating params
                             cost = -total_yield + physics_penalty
 
